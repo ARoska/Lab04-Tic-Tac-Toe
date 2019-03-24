f77a216d36705cb750c5af066e2c1fc254ea856c
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-24 10:27:57 -0700

    Finishes method to display board and starts method to play game.

--- Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab04_TicTacToe.Classes;
+using System;
 
 namespace Lab04_TicTacToe
 {
@@ -6,12 +7,36 @@ namespace Lab04_TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             PlayGame();
         }
 
         static void PlayGame()
         {
+            Console.Write("Welcome to Tic-Tac-Toe!\n" +
+                "\n" +
+                "Please enter Player 1's name: ");
+            string playerOneInput = Console.ReadLine();
+
+            Console.Write("\n" +
+                "Please enter Player 2's name: ");
+            string playerTwoInput = Console.ReadLine();
+
+            Player playerOne = new Player
+            {
+                Name = playerOneInput,
+                Marker = "X",
+                IsTurn = true
+            };
+
+            Player playerTwo = new Player
+            {
+                Name = playerTwoInput,
+                Marker = "O",
+                IsTurn = false
+            };
+
+
+
             // TODO: Setup your game here. Create an introduction. 
             // Create your players, and instantiate your Game class. 
             // output to the console the winner
